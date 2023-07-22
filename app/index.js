2ec76324004d44a7fa04d14c8cf42323ce7d09ac
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-07-22 18:42:27 +0700

    add comment feature

--- app/index.js
+++ app/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const app = express();
 const videoRoutes = require("./router/videoRoutes");
+const commentRoutes = require("./router/commentRoutes");
 
 app.use(express.json());
 
 app.use("/v1/videos", videoRoutes);
+app.use("/v1/comments", commentRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("Not Found/Invalid route");
+});
 
 module.exports = app;
